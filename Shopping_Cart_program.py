ddb03586dd5d69f6ed7fb0f83db79e94e68adc90
IbrahimLone09 <ibrahimnazir00003120401@gmail.com>
2026-02-23 18:12:54 +0530

    file 2

---
+++ Shopping_Cart_program.py
@@ -0,0 +1,10 @@
+item = input("What item would you like to buy? ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+total = price * quantity
+print(f"you have bought {quantity} * {item}")
+print(f"your total is: ${total}")
+    
+
+
+
