743d3afcbd41bc7ea3cb5e582fae368dc398ee9d
IbrahimLone09 <ibrahimnazir00003120401@gmail.com>
2026-03-06 15:03:00 +0530

    simple contact sysytem

---
+++ Contact_book.py
@@ -0,0 +1,50 @@
+contacts = {}
+
+print("Welcome to contact book!")
+
+while True:
+    print("Type '1' to Add contact:")
+    print("Type '2' to View contact:")
+    print("Type '3' to Search contact:")
+    print("Type '4' to Delete contact:")
+
+    choice = input("Enter your choice (Q to quit!): ").lower()
+
+    if choice == "q":
+        print("Exiting contact book!")
+        break
+
+    elif choice == "1":
+        name = input("Enter name: ")
+        phone = input("Enter phone number: ")
+        contacts[name] = phone
+        print("Contact added successfully!")
+
+    elif choice == "2":
+        if  contacts:
+            print("Contacts!")
+            for name , phone in contacts.items():
+             print(name , ":", phone)
+
+        else:
+            print("No contact found!")
+
+    elif choice == "3":
+        name = input("Enter name to search: ")
+        if name in contacts:
+            print("phone number:",contacts[name])
+
+        else:
+            print("Contact not found!")
+
+    elif choice == "4":
+        name = input("Enter name to delete! ")
+        if name in contacts:
+            del contacts[name]
+            print("Contact deleted!")
+
+        else:
+            print("Contact not found!")
+
+    else:
+        print("Invalid choice please try again!")
